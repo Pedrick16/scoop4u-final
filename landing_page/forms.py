fee1f2b1ded6d6409bca88322b9c53dd19d4bb03
Cess <95141065+cesstuvw@users.noreply.github.com>
2023-05-06 11:44:30 +0800

    Update forms.py

--- landing_page/forms.py
+++ landing_page/forms.py
@@ -89,11 +89,11 @@ class ContactForm(forms.Form):
 class ReviewForm(forms.Form):
     CHOICES = [
         ('0', ''),
-        ('1', '1'),
-        ('2', '2'),
-        ('3', '3'),
-        ('4', '4'),
         ('5', '5'),
+        ('4', '4'),
+        ('3', '3'),
+        ('2', '2'),
+        ('1', '1'),
     ]
     first_name = forms.CharField(max_length = 50, widget=forms.TextInput(attrs={'class' : 'form-control'}))
     last_name = forms.CharField(max_length = 50, widget=forms.TextInput(attrs={'class' : 'form-control'}))
